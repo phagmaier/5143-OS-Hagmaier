619ff81442ddb89bf5fa479bc4d42e4515c182d9
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-02-22 15:51:13 -0800

    Create Colors.py

---
+++ Assignments/Terminal/Colors.py
@@ -0,0 +1,20 @@
+'''
+ Program:       Terminal
+ Author:        Found on stack overflow: https://stackoverflow.com/questions/63768372/color-codes-for-discord-py
+ Date:          February 20, 2022
+Decription: This is a class that allows the color of whatever is printed to change. This code is not mine it was found in the link posted
+above in the author section I had no idea how to do this.
+'''
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
