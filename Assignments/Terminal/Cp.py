2e16538b433b8741f7872463b14739ea2823deb1
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-02-22 15:58:45 -0800

    Update Cp.py

--- Assignments/Terminal/Cp.py
+++ Assignments/Terminal/Cp.py
@@ -1,18 +1,17 @@
-''
+'''
  Program:       Terminal
  Author:        Parker Hagmaier
  Date:          February 20, 2022
-Decription: This command changes the directory. Like all my commands/functions
-it takes it wasPiped and args. arg will contain the directory we are going to cd into
-along with if it should be pipedOrRedirected although in a  real terminal
-i'm unsure if you can pipe into cd and or pipeOrRedirect cd but i allowd it and
-for pipe or re i just pass it the directory. The count variable makes sure nothing more than 
-the directory is passed to args and the obligitory wasPiped boolean value. other than that
-the function uses os.chdir to change the path and it automatically works for .. and . 
-and for ~ we use os.path.expandesure in order to indicate that means take me to the home directory. 
-we use try in order to make sure that if the directory is not a valid dir we will only get an error
-message and our program will not crash  
+Decription: This is the cp command which will not only copy files but will change the name if no directory is
+given for it to change in to. With all my commands it takes wasPiped and args. We parse the args to check for 
+if it should be piped (this is one of those cases where I don't know what to do if it is piped if it is piped
+I return the file not sure how this works in a real piped cp command) 
+As with the real cp command we can rename in mass so we put everything that is to be copied in a list named copy
+(we do not support mass renaming but it won't crash the program rather it will through up an error message)
+we know it is to be renamed and not copied if what is passed into args is both not a file and not a directory
+if both copyto and renmae variable == ''. shutil.copy is used to copy os.rename is used to rename. 
 '''
+
 import os
 import shutil
 def cp(wasPiped=None, *args):
