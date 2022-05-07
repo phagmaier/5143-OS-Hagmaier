51a858e54137604c9974752c393aabcb36c28d74
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-05-07 02:03:40 -0700

    Update new.py

--- Cpu Scheduling - Simulation/new.py
+++ Cpu Scheduling - Simulation/new.py
@@ -1,7 +1,12 @@
+'''
+When a job is loaded into our similation it is first moved to this queue
+'''
 class New:
 	def __init__(self):
 		self.queue = []
-
+	'''
+	add to new queue takes in a job and appends to queue
+	'''
 	def addNew(self, job):
 		self.job = job
 		self.queue.append(self.job)
