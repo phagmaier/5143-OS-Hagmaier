6445438dd3000333ef1d115bccf3179e422bd12f
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-05-07 02:00:49 -0700

    Update ioClass.py

--- Cpu Scheduling - Simulation/ioClass.py
+++ Cpu Scheduling - Simulation/ioClass.py
@@ -1,3 +1,10 @@
+'''
+Decrements the iobursts
+job saves the job loaded. Busy indicates if a job is in the IO
+send is a bollean that changes when a job needs to be sent to ready when finished.
+move is the job to move. pause means an action has been taken in this clock tick.
+'''
+
 class IO:
 	def __init__(self):
 		self.job = None
@@ -5,12 +12,18 @@ class IO:
 		self.send = False
 		self.move = None
 		self.pause = False
-
+	
+	'''
+	Recieves a job 
+	'''
 	def recieve(self, job):
 		if job != None:
 			self.job = job
 			self.busy = True
-
+	
+	'''
+	decrements IO bursts and sets proper boolean values so no other value can be loaded during this clock tick
+	'''
 	def moveToReady(self):
 		self.job.inputBursts = self.job.inputBursts[1:]
 		self.send = True
@@ -19,13 +32,18 @@ class IO:
 		self.busy = False
 		self.pause = True
 		return self.move
-
+	
+	'''
+	decrements IO burst
+	'''
 	def run(self):
 		self.job.inputBursts[0] -= 1
 		if self.job.inputBursts[0] == 0:
 			return self.moveToReady()
 
-
+	'''
+	resets for a new job to be loaded
+	'''
 	def reset(self):
 		self.pause = False
 
