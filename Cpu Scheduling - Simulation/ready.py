be84f9bb977af795c7e38f18c19a25070de20382
Parker Hagmaier <97493681+phagmaier@users.noreply.github.com>
2022-05-07 02:09:04 -0700

    Update ready.py

--- Cpu Scheduling - Simulation/ready.py
+++ Cpu Scheduling - Simulation/ready.py
@@ -1,10 +1,15 @@
+'''
+Initilized as an empty list to store values waiting to use CPU
+'''
 class Ready:
 	def __init__(self):
 		self.readyQueue = []
 		self.send = False
 		self.move = None
 		self.job = None
-	
+	'''
+	when added to ready we store values of the jobs and sent moved to True so no other action can be taken in the clock tick
+	'''
 	def addReady(self, jobs, algo):
 		self.send = True
 		for i in jobs:
@@ -13,7 +18,9 @@ class Ready:
 			self.job.moved = True
 			self.readyQueue.append(self.job)
 		self.sort(algo)
-
+	'''
+	depending on the algorithm the corresponding sort function will be called.
+	'''
 	def sort(self, algo):
 		if algo == 'sjf':
 			return self.sjf()
@@ -27,17 +34,26 @@ class Ready:
 			pass
 		else:
 			print('Not built yet!')
-
+	'''
+	sorts the readyQueue by prioirty
+	'''
 	def pb(self):
 		self.readyQueue.sort(key=lambda x: x.priority)
 		return self.readyQueue.reverse()
-
+	'''
+	sorts by the shortest total cpuBurst number
+	'''
 	def sjf(self):
 		return self.readyQueue.sort(key=lambda x: x.jobLength)
-
+	'''
+	sorts by the shortest remaining time of total cpuBursts remaining
+	'''
 	def srj(self):
 		return self.readyQueue.sort(key=lambda x: x.currentLength)
-
+	
+	'''
+	when the cpu is not busy this functin is called to send the first item in the queue to the cpu
+	'''
 	def sendCpu(self):
 		for i in range(len(self.readyQueue)):
 			if self.readyQueue[i].moved == False:
@@ -52,12 +68,16 @@ class Ready:
 				self.move = None 
 				return self.move
 
-
+	'''
+	increment the wait time for every job in the queue
+	'''
 	def cpuWait(self):
 		for i in self.readyQueue:
 			i.CPUWaitTime += 1
 			i.priority += .25
-
+	'''
+	when moved we call change to indicate an action was taken in the clock tick and another cannot be made
+	'''
 	def change(self):
 		if len(self.readyQueue) > 0:
 			for i in self.readyQueue:
